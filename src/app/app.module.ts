2a0ccefc1da6432d43b8496543a388b74dfce302
LuisFelipe333 <luigifelipe333@gmail.com>
2020-09-25 08:25:27 -0500

    add http client and create function request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component' ;
+
+import {HttpClientModule} from '@angular/common/http';
+
+import { CommonModule } from '@angular/common';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
