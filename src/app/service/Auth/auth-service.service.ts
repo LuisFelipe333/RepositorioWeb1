4513a0a00a1ebc1cdae182db6602c076e09cd0b0
LuisFelipe333 <luigifelipe333@gmail.com>
2020-11-30 21:16:51 -0600

    add dns

--- src/app/service/Auth/auth-service.service.ts
+++ src/app/service/Auth/auth-service.service.ts
@@ -11,7 +11,7 @@ import { Observable } from 'rxjs';
 
 export class AuthServiceService {
   //api: String = 'http://localhost:8000/';
-  api: String = 'https://backweb-ids.herokuapp.com/';
+  api: String = 'http://web-felipe.ddns.net/';
 
   constructor( private http: HttpClient ) { }
 
